a20b4796ab7ce96e3ca11e2f04a63c6e69c17ef3
Karan anand <karan.a4248@gmail.com>
2026-02-22 15:03:06 -0600

    made some changes to printing  and and added a Map class (should be teh grid I should rename it now)

---
+++ src/main/java/ca/umanitoba/cs/kanand/Map.java
@@ -0,0 +1,14 @@
+package ca.umanitoba.cs.kanand;
+
+public class Map{
+    class map {
+        private int rows;
+        private int cols;
+        public void setMap(int rows, int cols) {
+            this.rows = rows;
+            this.cols = cols;
+        }
+
+    }
+
+}
